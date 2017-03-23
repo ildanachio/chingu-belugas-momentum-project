822cf52f4a3583c620b3bd59714a5bdf0c851e54
Pankajashree <pankajashree@gmail.com>
2017-03-23 21:37:28 +0530

    Debug local storage issues

--- FocusOfTheDay/index.js
+++ FocusOfTheDay/index.js
@@ -4,12 +4,13 @@ var dayTask = document.getElementById("dayTask"); //text input
 var task = document.getElementById("task"); //task span
 var dayTasks = []; //array to store tasks 
 var cross = document.getElementById("cross"); //cross icon to cancel/delete the task
+var plus = document.getElementById("plus"); //cross icon to cancel/delete the task
+plus.style.display = 'none';
 inputDiv.style.display = 'none'; //initially hide input div
 taskDiv.style.display ='none'; //initially hide task display div
-//if(localStorage['task'] !== null) dayTasks.push(localStorage['task']);
 var checkBox = document.getElementById("check");
 
-if(!localStorage['task']){
+  if(!localStorage['task']){
     taskDiv.style.display ='none'; 
     inputDiv.style.display = 'block';
     dayTask.addEventListener('keydown', dayFocus);
@@ -17,6 +18,7 @@ if(!localStorage['task']){
   else{
       displayTask();
   }
+
 
 
 function dayFocus(e){
@@ -32,11 +34,12 @@ function dayFocus(e){
 
 function displayTask(){
         task.innerHTML = localStorage.getItem('task');
-        if(localStorage.textStyle){
+        if(localStorage['textStyle']){
           task.style.textDecoration = localStorage.getItem('textStyle');
         }
-        if(localStorage.chekbox){
-            checkBox.checked = (localStorage.getItem('checkbox') == 'true');
+        if(localStorage['checkbox']){
+            checkBox.checked = (localStorage.getItem('checkbox')) == 'true';
+            testCheck();
         } 
         taskDiv.style.display = 'inline';
         
@@ -46,12 +49,14 @@ function displayTask(){
                checkBox.checked == false;
                localStorage.setItem('checkbox','false');
                localStorage.setItem('textStyle','none');
+               testCheck();
           }
           else{
               task.style.textDecoration = 'line-through';
               checkBox.checked == true;
               localStorage.setItem('checkbox','true');
               localStorage.setItem('textStyle','line-through');
+              testCheck();
           }
         });
         
@@ -60,14 +65,27 @@ function displayTask(){
 }
 
 function deleteTask(){
-   
-     taskDiv.style.display = 'none';
-      localStorage.removeItem('task');
+    localStorage.removeItem('task');
       localStorage.removeItem('checkbox');
       localStorage.removeItem('textStyle');
+     taskDiv.style.display = 'none';
+     task.style.textDecoration = 'none';
+    checkBox.checked = false; 
+    testCheck();
     dayTask.value = '';
     inputDiv.style.display='block';
     dayTask.addEventListener('keydown', dayFocus);
 }
 
-
+function testCheck(){
+  if(checkBox.checked){
+    cross.style.display = 'none';
+    plus.style.display = 'inline';
+    plus.addEventListener('click', deleteTask);
+  }
+  else{
+    cross.style.display = 'inline';
+    plus.style.display = 'none';
+    cross.addEventListener('click', deleteTask);
+  }
+}
