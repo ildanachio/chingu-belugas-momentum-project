92cb18e295d7d29c570f9b4b457bf4c5b44d9836
Pankajashree <pankajashree@gmail.com>
2017-03-26 21:26:51 +0530

    updates

--- Chrome Ext/index.js
+++ Chrome Ext/index.js
@@ -1,36 +1,25 @@
 var weatherElement = document.getElementById("weather");
 //var weatherIcon  = document.getElementById("image");
-var city = document.getElementById("city");
-var dateDiv = document.getElementById("date");
-var description = document.getElementById("description");
-var wind = document.getElementById("wind");
-var humidity = document.getElementById("humidity");
-var settingsBtn = document.getElementById("settingsBtn");
-var settingsDisplay = document.getElementById("settings");
-var fahren;
+var city  = document.getElementById("city");
+var description  = document.getElementById("description");
+var wind  = document.getElementById("wind");
+var humidity  = document.getElementById("humidity");
 
-if(localStorage.fah){
-	fahren = localStorage["fah"];
-}
- else{
-	 fahren = false;
-	 localStorage.setItem('fah',fahren);
- }
- 
+var fahren = false;
 weatherModule();
-setInterval(weatherModule, 1800000);
+setInterval(weatherModule,1800000); 
 
 //Weather Module
 function weatherModule() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, errorCallback, { timeout: 10000 });
+        navigator.geolocation.getCurrentPosition(showPosition,errorCallback,{timeout:10000});
     } else {
         weatherElement.textContent = "Please allow sharing";
     }
 }
 
 //Handle Error
-function errorCallback(event) {
+function errorCallback(event){
     console.log("This is error handler");
     console.log(event);
     weatherElement.textContent = "error acquiring position";
@@ -38,94 +27,44 @@ function errorCallback(event) {
 
 //Get Location
 function showPosition(position) {
-    var latitude = position.coords.latitude;
+    var latitude  = position.coords.latitude;
     var longitude = position.coords.longitude;
     //displayPort.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
-    getWeather(latitude, longitude);
-
+    getWeather(latitude,longitude);
+   
 }
 
 //Get Weather
-function getWeather(lat, long) {
+function getWeather(lat,long) {
     //request Data
-    var url = 'https://api.apixu.com/v1/current.json?key=b1385ce3224e427e954191546172802&q=' + lat + ',' + long;
+    var url = 'https://api.apixu.com/v1/current.json?key=b1385ce3224e427e954191546172802&q=' + lat + ',' +long;
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url);
     xhr.onload = function() {
         var data = JSON.parse(xhr.responseText);
-        displayWeather(data, lat, long);
+        displayWeather(data,lat,long);
     };
     xhr.send(null);
 }
 
 //Display Weather
-function displayWeather(data, lat, long) {
+function displayWeather(data,lat,long){
     var temp;
-    
-    
-    if (localStorage.fah == "true" ) {
-		
+    if(fahren){
         temp = Math.ceil(data.current.temp_f) + "°F";
-    } 
+    }
     else{
         temp = Math.ceil(data.current.temp_c) + "°C";
     }
-    
-    
-    
-    /* if (fahren) {
-        temp = Math.ceil(data.current.temp_f) + "°F";
-    } else {
-        temp = Math.ceil(data.current.temp_c) + "°C";
-    } */
-  
-    
     city.innerHTML = data.location.name;
-    dateDiv.innerHTML = data.location.localtime;
     description.innerHTML = data.current.condition.text;
     wind.innerHTML = 'Wind: ' + data.current.wind_dir + ' ' + data.current.wind_mph + ' mph';
     humidity.innerHTML = 'Humidity: ' + data.current.humidity + '%';
     var iconSource = 'https:' + data.current.condition.icon;
-    weatherElement.textContent = temp;
-    document.getElementById("image").src = iconSource;
-
-}
-
-//click SettingsBtn
-settingsDisplay.style.display = 'none';
-settingsBtn.onclick = function() {
-    showSettings();
-};
-
-//Show Settings
-function showSettings() {
-    if (settingsDisplay.style.display === 'none') {
-        settingsDisplay.style.display = 'block';
-    } else {
-        settingsDisplay.style.display = 'none';
-    }
-}
-
-//choosing units
-document.getElementById("c").onclick = function() {
-    fahren = false;
-	saveChanges(fahren);
+    weatherElement.textContent =  temp;
+    document.getElementById("image").src=iconSource;
     
-};
-
-document.getElementById("f").onclick = function() {
-    fahren = true;
-	saveChanges(fahren);
-   
-};
-
-
-
-function saveChanges(fah){
-    localStorage.setItem('fah',fah);
-    console.log(localStorage.fah);
-	weatherModule();
 }
-
-
-
+ 
+  
+  
