6682d7b566c949acaf3353a0040d22bd46948d4e
Authorauthor <authorsofreality@gmail.com>
2017-04-05 19:24:59 +1000

    Fixed bug that allowed blank list items

--- todo.js
+++ todo.js
@@ -9,13 +9,13 @@ function get_todos() {
 
 function add() {
     var task = document.getElementById('tdTask').value;
-
+if ($('#task').val() !== "") {
     var todos = get_todos();
     todos.push(task);
     localStorage.setItem('todo', JSON.stringify(todos));
 
     show();
-
+}
     return false;
 }
 
